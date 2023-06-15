92663c19864ae0339e9fba3fc02b8d2b23b0189d
chauchuty <cesar.chauchuty@gmail.com>
2023-06-15 20:02:38 -0300

    updated

--- app-back/src/controllers/occurrence.controller.js
+++ app-back/src/controllers/occurrence.controller.js
@@ -12,6 +12,10 @@ export function getOcurrences(req, res) {
 
 export function newOccurrences(req, res) {
     const data = req.body
+    
+    if(new Date(data.registered_at) > new Date()){
+        return res.status(400).json({ message: 'Data de registro não pode ser maior que a data atual!'})
+    }
     OccurrenceService.create(data)
         .then(response => {
             res.status(200).json({ message: 'Ocorrência cadastrada com sucesso!'})
